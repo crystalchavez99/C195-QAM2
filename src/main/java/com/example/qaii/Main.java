0f9a3761097080937a736118853d1828223e6d88
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-07-07 10:28:26 -0700

    Added dependicies

--- src/main/java/com/example/qaii/Main.java
+++ src/main/java/com/example/qaii/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     public static void main(String[] args) throws Exception {
         DAO.startCon();
-        launch();
+        launch(args);
         DAO.closeCon();
     }
 }
