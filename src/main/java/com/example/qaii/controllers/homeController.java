63755cb4b1a0855ae175e6adad6c4e6d4547a1d9
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-07-07 15:50:55 -0700

    home view opens contacts and appointments

--- src/main/java/com/example/qaii/controllers/homeController.java
+++ src/main/java/com/example/qaii/controllers/homeController.java
@@ -1,10 +1,16 @@
 package com.example.qaii.controllers;
 
+import com.example.qaii.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.Node;
+
+import java.io.IOException;
+
 public class homeController {
     @FXML
     private Button logoutButton;
@@ -12,5 +18,23 @@ public class homeController {
     public void logoutButton(ActionEvent event){
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
+    }
+
+    public void openAppointments(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("appointmentView.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Appointments");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void openContacts(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("contactView.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Contacts");
+        stage.setScene(scene);
+        stage.show();
     }
 }
