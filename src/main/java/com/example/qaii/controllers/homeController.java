8589d2ff258f0363f8ef6a2fc4840cb7655bbd35
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-07-07 14:58:10 -0700

    home view works

--- src/main/java/com/example/qaii/controllers/homeController.java
+++ src/main/java/com/example/qaii/controllers/homeController.java
@@ -1,4 +1,16 @@
 package com.example.qaii.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 public class homeController {
+    @FXML
+    private Button logoutButton;
+
+    public void logoutButton(ActionEvent event){
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
 }
