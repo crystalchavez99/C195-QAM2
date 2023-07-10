7de1107191dd6f570c147463801a565d0da6f8be
crystalchavez99 <redfire1205@hotmail.com>
2023-07-09 22:23:08 -0700

    Added DAO For Appointments

--- src/main/java/com/example/qaii/database/AppointmentDB.java
+++ src/main/java/com/example/qaii/database/AppointmentDB.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class AppointmentDB {
 
@@ -22,6 +23,12 @@ public class AppointmentDB {
             String appointment_description = rs.getString("description");
             String appointment_location = rs.getString("location");
             String appointment_type = rs.getString("type");
+            LocalDateTime start = rs.getTimestamp("start").toLocalDateTime();
+            LocalDateTime end = rs.getTimestamp("end").toLocalDateTime();
+            int customer_id = rs.getInt("customer_id");
+            int user_id = rs.getInt("User_id");
+            int contact_id = rs.getInt("Contact_id");
         }
+        return allAppointments;
     }
 }
