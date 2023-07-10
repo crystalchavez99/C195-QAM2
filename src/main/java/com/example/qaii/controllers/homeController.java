ce6b322d7d0282159772f564009c4af9d8b944f1
crystalchavez99 <redfire1205@hotmail.com>
2023-07-09 22:27:43 -0700

    start displaying appointment check

--- src/main/java/com/example/qaii/controllers/homeController.java
+++ src/main/java/com/example/qaii/controllers/homeController.java
@@ -1,6 +1,9 @@
 package com.example.qaii.controllers;
 
 import com.example.qaii.Main;
+import com.example.qaii.database.AppointmentDB;
+import com.example.qaii.models.Appointment;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,10 +14,24 @@ import javafx.scene.Node;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class homeController {
+
     @FXML
     private Button logoutButton;
+
+    public void initialize() throws SQLException, IOException, Exception  {
+        try{
+            ObservableList<Appointment> getAllAppointments = AppointmentDB.getAllAppointments();
+            LocalDateTime before15Min = LocalDateTime.now().minusMinutes(15);
+            LocalDateTime after15Min = LocalDateTime.now().plusMinutes(15);
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 
     public void logoutButton(ActionEvent event){
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
