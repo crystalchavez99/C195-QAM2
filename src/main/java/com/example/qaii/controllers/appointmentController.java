29469074764e328ed0cdd99948064fff4e6e7e5b
crystalchavez99 <redfire1205@hotmail.com>
2023-07-16 21:51:43 -0700

    models added and work on appointment table controller

--- src/main/java/com/example/qaii/controllers/appointmentController.java
+++ src/main/java/com/example/qaii/controllers/appointmentController.java
@@ -3,6 +3,7 @@ package com.example.qaii.controllers;
 import com.example.qaii.DAO;
 import com.example.qaii.database.AppointmentDB;
 import com.example.qaii.models.Appointment;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,12 +13,18 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class appointmentController {
 
     @FXML
     private TableView<Appointment> appointmentTable;
+
+    ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
 
     @FXML
     private TableColumn<?, ?> appointment_id;
@@ -40,6 +47,8 @@ public class appointmentController {
     @FXML
     private TableColumn<?, ?> user_id;
 
+    private final DateTimeFormatter datetimeDTF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     @FXML
     public void backButton(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -61,13 +70,52 @@ public class appointmentController {
         user_id.setCellValueFactory(new PropertyValueFactory<>("user_id"));
         appointmentTable.setItems(getAllAppointments);
 
+
+    }
+
+    public void setAppointmentTable() throws SQLException{
+        PreparedStatement ps;
         try{
-            DAO.startCon();
+            ps = DAO.startCon().prepareStatement(
+                    "SELECT appointment.appointment_id, appointment.customer_id, appointment.user_id, appointment.title, appointment.description, "
+                            + "appointment.location, appointment.contact, appointment.type, appointment.start, appointment.end, "
+                            + "appointment.created_by, customer.customer_id, customer.customer_name "
+                            + "FROM appointment, customer "
+                            + "WHERE appointment.customer_id = customer.customer_id "
+                            + "ORDER BY `start`");
+            ResultSet rs = ps.executeQuery();
+            appointmentList.clear();
+            while (rs.next()) {
+                int appointmentID = rs.getInt("appointment_id");
+                int customerID = rs.getInt("customer_id");
+                int userID = rs.getInt("user_id");
+                String description = rs.getString("description");
+                String location = rs.getString("location");
+                String contact = rs.getString("contact");
 
+                //get database start time stored as UTC
+                String startUTC = rs.getString("start").substring(0, 19);
 
+                //get database end time stored as UTC
+                String endUTC = rs.getString("end").substring(0, 19);
 
-        }catch{
+                //convert database UTC to LocalDateTime
+                LocalDateTime utcStartDT = LocalDateTime.parse(startUTC, datetimeDTF);
+                LocalDateTime utcEndDT = LocalDateTime.parse(endUTC, datetimeDTF);
 
+                //get title from appointment
+                String title = rs.getString("title");
+
+                //get type from appointment
+                String type = rs.getString("type");
+
+                //put Customer data into Customer object
+                Customer customer = new Customer(rs.getInt("customer_id"), rs.getString("customer_name"));
+                String customerName = customer.getCustomerName();
+            }
+        } catch (Exception e) {
+            System.out.println("Something other than SQL has caused an error!");
         }
+
     }
 }
